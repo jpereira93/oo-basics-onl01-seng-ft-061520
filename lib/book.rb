532e39e3030ae4be9f748aff7716b57439ba2de2
Jessica Pereira <jcpereira070@gmail.com>
2020-06-23 17:51:53 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+class Book
+  Book.new("And Then There Were None")
+end
 
 
+
+
+
+  # new
+  #   gets initialized with a title (FAILED - 1)
+  
+  # properties
+  #   has a title (FAILED - 2)
+  #   has an author name (FAILED - 3)
+  #   has a page count (FAILED - 4)
+  #   has a genre (FAILED - 5)
+
+  
+  # #turn_page
+  #   can turn the page (FAILED - 6)
