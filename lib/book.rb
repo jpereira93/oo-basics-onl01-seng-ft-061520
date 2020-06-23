08d9314020fbf839f303ec08e021bdb4e565505a
Jessica Pereira <jcpereira070@gmail.com>
2020-06-23 17:51:56 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,13 +8,13 @@ end
 
   # new
   #   gets initialized with a title (FAILED - 1)
-  
+
   # properties
   #   has a title (FAILED - 2)
   #   has an author name (FAILED - 3)
   #   has a page count (FAILED - 4)
   #   has a genre (FAILED - 5)
 
-  
+
   # #turn_page
   #   can turn the page (FAILED - 6)
